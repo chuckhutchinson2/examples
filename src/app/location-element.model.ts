4446fc8474077681a5e9466d93a3cc61f7d6bee0
Chuck Hutchinson <chuck@perfectsensedigital.com>
2018-09-28 11:50:22 -0400

    added model for interface

---
+++ src/app/location-element.model.ts
@@ -0,0 +1,13 @@
+export interface LocationElement {
+  areaCode: string;
+  city: string;
+  county: string;
+  daylightSavingsTime: string;
+  fips: string;
+  latitude: string;
+  longitude: string;
+  state: string;
+  timeZone: string;
+  weatherCode: string;
+  zip: string;
+}
